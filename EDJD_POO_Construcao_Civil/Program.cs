d3e40e8bef50045b788bbe3d2e15a48780a38ca3
Vulpix98 <leonelqo@hotmail.com>
2023-11-20 12:33:15 +0000

    Add project files.

---
+++ EDJD_POO_Construcao_Civil/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EDJD_POO_Construcao_Civil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
